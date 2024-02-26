e836c51599fe2ebaf73bbc21c5f03ebc049b9c1b
tfeadzwa <tfadzwa02@gmail.com>
2024-02-26 23:59:35 +0200

    Add contact and articles components to the header feature

---
+++ src/components/articles/Articles.jsx
@@ -0,0 +1,7 @@
+const Articles = () => {
+  return (
+    <section className="articles" style={{ color: "white", fontSize: "2.4rem" }}>Articles</section>
+  );
+};
+
+export default Articles;
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <section className="contact" style={{ color: "white", fontSize: "2.4rem" }}>
+      Contact
+    </section>
+  );
+};
+
+export default Contact;
