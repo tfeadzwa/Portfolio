8639ce79f1be66291186a5c25929a10da1255227
tfeadzwa <tfadzwa02@gmail.com>
2024-03-07 23:46:54 +0200

    Add hover animations on project item hovering

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -12,24 +12,35 @@ const Projects = () => {
 
         <div className="projects__content border-line-v pos-relative">
           <div className="projects__items-container">
-            <div className="projects__item-container">
+            <div className="projects__items">
               <div className="projects__item">
-                <div className="projects__item-image">
-                  <Link>
-                    <img src="/images/work-r2.jpg" alt="projects__item-image" />
-                    <span className="projects__item-info">
-                      <span className="projects__eye-icon"></span>
-                    </span>
-                  </Link>
-                </div>
-                <div className="projects__item-desc">
-                  <div className="projects__desc-info">
-                    <div>MENCRUIT</div>
-                    <div>MenCruit Rucruitment Web application System</div>
+                <Link className="projects__image-link">
+                  <div className="projects__image">
+                    <img src="/images/work-r2.jpg" />
                   </div>
-                  <a href="https://www.github.com/Tfeadzwa">
-                    <FontAwesomeIcon icon={["fab", "github"]} />
-                  </a>
+                </Link>
+                <div className="projects__desc">
+                  <div className="projects__name">
+                    <span>Content</span>
+                  </div>
+                  <div className="projects__title">
+                    <span>Cryptocurrency Dashboard Application</span>
+                  </div>
+                </div>
+              </div>
+              <div className="projects__item">
+                <Link className="projects__image-link">
+                  <div className="projects__image">
+                    <img src="/images/work3-1.jpg" />
+                  </div>
+                </Link>
+                <div className="projects__desc">
+                  <div className="projects__name">
+                    <span>Content</span>
+                  </div>
+                  <div className="projects__title">
+                    <span>Cryptocurrency Dashboard Application</span>
+                  </div>
                 </div>
               </div>
             </div>
