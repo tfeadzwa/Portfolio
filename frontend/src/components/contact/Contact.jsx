90df63141bbe15f09c93e9a9515bb45183217991
tfeadzwa <tfadzwa02@gmail.com>
2024-03-21 18:05:09 +0200

    Add user contact form input validations with Yup and formik libraries

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -1,24 +1,8 @@
+import ContactForm from "./ContactForm";
 import MapLocation from "./MapLocation";
 import "./contact.scss";
-import ReCAPTCHA from "react-google-recaptcha";
-import { useRef } from "react";
-import axios from "axios";
 
 const Contact = () => {
-  const captchaRef = useRef(null);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log("Submit");
-    const token = captchaRef.current.getValue();
-    captchaRef.current.reset();
-
-    await axios
-      .post("http://localhost:3500/post", { token })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err.message));
-  };
-
   return (
     <section className="contact">
       <div className="contact__container">
@@ -62,56 +46,7 @@ const Contact = () => {
         <div className="contact__form-title title border-line-h pos-relative">
           <span>Contact Form</span>
         </div>
-
-        <form onSubmit={handleSubmit} className="contact__form">
-          <div className="contact__form-inputs">
-            <input
-              type="text"
-              className="contact__form-full-name"
-              id="full-name"
-              placeholder="Full Name"
-            />
-            <input
-              type="text"
-              className="contact__form-email"
-              id="email-address"
-              placeholder="Email Address"
-            />
-          </div>
-
-          <textarea
-            className="contact__form-message"
-            placeholder="Your Message"
-          />
-
-          <p>Please verify that you are not a robot to proceed:</p>
-
-          <ReCAPTCHA
-            sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
-            ref={captchaRef}
-          />
-
-          <button
-            type="button"
-            onClick={handleSubmit}
-            id="send-button"
-            className="contact__form-send-button send-button"
-          >
-            <span>SEND MESSAGE</span>
-
-            <svg
-              className="contact__arrow"
-              viewBox="0 0 25 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M16.67 12.5825L5.5 12.5825C4.95 12.5825 4.5 12.1325 4.5 11.5825C4.5 11.0325 4.95 10.5825 5.5 10.5825L16.67 10.5825L11.79 5.7025C11.4 5.3125 11.4 4.6825 11.79 4.2925C12.18 3.9025 12.81 3.9025 13.2 4.2925L19.79 10.8825C20.18 11.2725 20.18 11.9025 19.79 12.2925L13.2 18.8825C13.0132 19.0698 12.7595 19.175 12.495 19.175C12.2305 19.175 11.9768 19.0698 11.79 18.8825C11.4 18.4925 11.4 17.8525 11.79 17.4625L16.67 12.5825Z"
-                fill="currentColor"
-              />
-            </svg>
-          </button>
-        </form>
+        <ContactForm />
       </div>
     </section>
   );
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Header } from "./header/Header";
-export { default as Profile } from "./profile/Profile";
-export { default as About } from "./about/About";
-export { default as Projects } from "./projects/Projects";
-export { default as Resume } from "./resume/Resume";
-export { default as Contact } from "./contact/Contact";
-export { default as Articles } from "./articles/Articles";
